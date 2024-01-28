ddd85d30b989eec645be60b359befe38be4d1f1e
Maxwell <lubalecliff@gmail.com>
2024-01-28 14:34:54 +0300

    Completed Assignment

--- server/models.py
+++ server/models.py
@@ -6,25 +6,33 @@ class Author(db.Model):
     __tablename__ = 'authors'
     
     id = db.Column(db.Integer, primary_key=True)
-    name= db.Column(db.String, unique=True, nullable=False)
+    name = db.Column(db.String, unique=True, nullable=False)
     phone_number = db.Column(db.String)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
     # Add validators 
     @validates('name')
-    def validate_name(self,key,name):
+    def validate_name(self, key, name):
         if not name:
-            raise ValueError("All authors have a name") 
+            raise ValueError("All authors must have a name.")
+
+        # Check for uniqueness
+        existing_author = Author.query.filter(Author.name == name).first()
+        if existing_author and existing_author.id != self.id:
+            raise ValueError("Author name must be unique.")
+
         return name
+
     @validates('phone_number')
-    def validate_phone_number(self,key,phone_number):
-        if phone_number and not phone_number.isdigit() or len(phone_number)!=10:
+    def validate_phone_number(self, key, phone_number):
+        if phone_number and (not phone_number.isdigit() or len(phone_number) != 10):
             raise ValueError("Author phone numbers are exactly ten digits.")
         return phone_number
 
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
+
 
 class Post(db.Model):
     __tablename__ = 'posts'
@@ -46,15 +54,17 @@ class Post(db.Model):
 
     @validates('summary')
     def validate_summary(self, key, summary):
-        if len(summary) > 250:
+        if summary and len(summary) > 250:
             raise ValueError("Post summary must be at most 250 characters long.")
         return summary
+    
     @validates('category')
     def validate_category(self, key, category):
         valid_categories = ['Fiction', 'Non-Fiction']
         if category not in valid_categories:
             raise ValueError("Post category must be either Fiction or Non-Fiction.")
         return category
+    
     @validates('title')
     def validate_title(self, key, title):
         clickbait_keywords = ["Won't Believe", "Secret", "Top", "Guess"]
@@ -62,6 +72,5 @@ class Post(db.Model):
             raise ValueError("Post title must contain one of the following: 'Won't Believe', 'Secret', 'Top', 'Guess'.")
         return title
 
-
     def __repr__(self):
-        return f'Post(id={self.id}, title={self.title} content={self.content}, summary={self.summary})'
+        return f'Post(id={self.id}, title={self.title}, content={self.content}, summary={self.summary})'
